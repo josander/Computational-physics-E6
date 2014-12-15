ac663f24644e7c419ad4ade5f5ff011f0cfbedbd
Josephine <jossan_cuellar@hotmail.com>
2014-12-15 11:07:33 +0100

    New variables

--- E6_func.c
+++ E6_func.c
@@ -7,17 +7,6 @@ E6_func.c
 #include <stdlib.h>
 #define PI 3.141592653589
 
-double acceleration(double v, double x, double ny, double omega){
-
-	double a;
-	double eta;
-
-	eta = (double) rand() / RAND_MAX;
-	a = -ny * v - omega*omega*x + eta;
-
-	return a;
-
-}
 
 
 
