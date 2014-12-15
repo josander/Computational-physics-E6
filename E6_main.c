5852364ab8c169ef6676718d00b23e368f727071
Josephine <jossan_cuellar@hotmail.com>
2014-12-15 11:10:48 +0100

    Save phi in file

--- E6_main.c
+++ E6_main.c
@@ -22,6 +22,7 @@ int main()
 	double l;
 	double d;
 	double r_c, r_plus, r_minus;
+	double phi;
 
 	// Initiation of variables
 	lambda = 1;
@@ -37,11 +38,14 @@ int main()
 	file = fopen("phi.data","w");
 
 	// Get phi
+	phi = 0;
 	//phi = - lambda / (2 * PI * epsilon0) * (log(r - r+) - log(r - r-));
 
+	fprintf(file,"%f\n", phi);
+
 	
-	
-	
+	// Close file
+	fclose(file);
 
 }
 
