632169d651f0ab2ceb82ba1b36a707d039986680
David <david.emil.svensson@gmail.com>
2014-12-15 14:01:37 +0100

    fast

--- plot_funcE6.m
+++ plot_funcE6.m
@@ -1,14 +1,14 @@
 %% E6
 
-phi = importdata('phi.data');
-
+phi = dlmread('phi.data', '\t');
+phi = phi(1:end-1);
 
 %% Plot dipole solution 
 L = 1;
 Lint = 0.00001;
 poleDisp = 0.1*L;
 
-X = 0:Lint:L;
+X = 0:Lint:L - Lint;
 
 phiDipole = @(x) -(log(abs(x - L/2 - poleDisp)) - log(abs(x - L/2 + poleDisp)))/(2*pi);
 
